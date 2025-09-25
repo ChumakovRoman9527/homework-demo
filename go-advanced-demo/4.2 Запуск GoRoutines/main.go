41895fa8407638fb873596c2fcc32ab7f7c04fa5
Roman Chumakov <chumakoff@list.ru>
2025-09-25 07:26:03 +0300

    запустил многопоточку, но как контролить пока не понятно

--- go-advanced-demo/4.2 Запуск GoRoutines/main.go
+++ go-advanced-demo/4.2 Запуск GoRoutines/main.go
@@ -2,17 +2,39 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 )
 
-func main() {
-	go printHi()
-	go fmt.Println("привет из main")
-	go fmt.Println("привет из main2")
-	time.Sleep(time.Second)
+//10 запросов GET по адресу google.com
+// StatusCode вывести в консоле
 
+func main() {
+	t := time.Now()
+	// all_t := 0.00
+	// curr_t := 0
+	for i := 1; i < 10; i++ {
+		// curr_t = 0
+		// curr_t =
+		go getStatusCode(i)
+		// all_t = all_t + float64(curr_t)
+	}
+	time.Sleep(time.Second)
+	fmt.Println(time.Since(t))
 }
 
-func printHi() {
-	fmt.Println("привет из gr")
+func getStatusCode(i int) int64 {
+	t := time.Now()
+	var curr_t int64
+	fmt.Println("Начало запроса - ", i)
+	rasp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Ошибка запроса!!! ", i)
+		return 0
+	}
+	fmt.Println("Результат запроса - ", i, rasp.StatusCode)
+	fmt.Println(time.Since(t))
+
+	curr_t = time.Since(t).Milliseconds()
+	return curr_t
 }
