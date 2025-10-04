cec920a5279423cf081f9a03de6b6f179add444c
Roman Chumakov <chumakoff@list.ru>
2025-10-04 09:42:03 +0300

    Сделал 2

--- go-advanced-DZ/1-Concurrency/main.go
+++ go-advanced-DZ/1-Concurrency/main.go
@@ -5,12 +5,37 @@ import (
 	"math/rand/v2"
 )
 
-func generateNum(numChan chan int, max int, min int) {
-	res := -1
+func generateNum(max int, min int) int {
 
-	res = min + rand.IntN(max-min)
+	res := min + rand.IntN(max-min)
 
-	numChan <- res
+	return res
+}
+
+func generateSlice(size int, max int, min int, numChan chan int) {
+	defer close(numChan)
+	res := make([]int, size)
+
+	for i := 0; i < size; i++ {
+		res[i] = generateNum(max, min)
+	}
+
+	for _, val := range res {
+
+		numChan <- val
+	}
+
+}
+
+func consoleSquare(numChan chan int, squareChan chan int) {
+
+	for iter := range numChan {
+
+		squareChan <- squareNum(iter)
+
+	}
+	close(squareChan)
+
 }
 
 func squareNum(num int) int {
@@ -18,15 +43,22 @@ func squareNum(num int) int {
 }
 
 func main() {
+	//Определяем размер каналов, масимум и минимум для случайных чисел
 	chanBuff := 10
+	max := 100
+	min := 0
+	// Создаем каналы
 	numChan := make(chan int, chanBuff)
+	squareChan := make(chan int, chanBuff)
+	//Запускаем горутину генерации случайных чисел, создания слайса
+	go generateSlice(chanBuff, max, min, numChan)
+	//Запускаем горутину возведения в квадрат случайных чисел ищ горутины 1
+	go consoleSquare(numChan, squareChan)
+	//ожидаем когда канал квадратных чисел закроется, а пока ждем последовательно выводим в консоль
+	//важно ! не понял почему если сделать слепой range а в теле цикла выводить извлекая из канала значение, канал будет перебран неполностью ! вероятно из за рассинхронизации
+	for val := range squareChan {
 
-	for i := 0; i < chanBuff; i++ {
-		go generateNum(numChan, 10, 0)
-	}
-
-	for range chanBuff {
-		fmt.Printf("%d ", squareNum(<-numChan))
+		fmt.Printf("%d ", val)
 	}
 
 }
