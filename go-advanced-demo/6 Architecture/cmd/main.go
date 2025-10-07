0fa9d72c8f8fc6f426a5c13b32c1cf99a4d6b7f4
Roman Chumakov <chumakoff@list.ru>
2025-10-07 05:56:14 +0300

    changes previous simple app into better folder structure

---
+++ go-advanced-demo/6 Architecture/cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"5.2-SimpleHTTPServer/internal/hello"
+)
+
+func main() {
+	router := http.NewServeMux()
+	hello.NewHelloHandler(router)
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("Server is listening on port 8081")
+	//http.ListenAndServe(":8081", nil)
+	server.ListenAndServe()
+
+}
