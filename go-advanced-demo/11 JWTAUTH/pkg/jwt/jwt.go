7d56090be39fd129beb33460f97c1c3e6ac74ce7
Roman Chumakov <chumakoff@list.ru>
2025-11-17 06:23:00 +0300

    JWT 1

---
+++ go-advanced-demo/11 JWTAUTH/pkg/jwt/jwt.go
@@ -0,0 +1,27 @@
+package jwt
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWT struct {
+	Secret string
+}
+
+func newJWT(secret string) *JWT {
+	return &JWT{
+		Secret: secret,
+	}
+}
+
+func (j *JWT) Create(email string) (string, error) {
+	// jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	claims := jwt.MapClaims{"email": email}
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	s, err := t.SignedString([]byte(j.Secret))
+	if err != nil {
+		return "", err
+	}
+
+	return s, nil
+}
